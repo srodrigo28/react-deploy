adb2472818ca01a57be4c329405986c97fcef4e7
SEBASTIAO RODRIGO <rodrigoexer1@gmail.com>
2024-03-09 11:01:08 -0300

    updated

--- src/views/Login/index.jsx
+++ src/views/Login/index.jsx
@@ -1,4 +1,15 @@
+import { toast } from 'react-toastify';
+
 export function Login(){
+
+    const Entrar = () => {
+        toast("Usuário ou senha não conferi :(")
+    }
+
+    function Registar() {
+        toast("Servidor offline volte mais tarde ...")
+    }
+
     return(
         <div className="container text-center">
             <div className="form col-6 mx-auto">
@@ -8,12 +19,12 @@ export function Login(){
                         <input type="text" className="form-control" placeholder="e-mail" />
                     </div>
                     <div className="col">
-                        <input type="password" className="form-control" placeholder="*******" />
+                        <input type="password" className="form-control text-center" placeholder="*******" />
                     </div>
                     <div className="row">
                         <div className="col p-3 d-flex gap-3">
-                            <button className="btn btn-outline-primary w-25">Entrar</button>
-                            <button className="btn btn-outline-success w-25">Entrar</button>
+                            <button onClick={Entrar} className="btn btn-outline-primary w-25">Entrar</button>
+                            <button onClick={Registar} className="btn btn-outline-success w-25">Registar</button>
                         </div>
                     </div>
                 </div>
