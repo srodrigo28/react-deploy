93b1aa29f2e62f26ca3093e2af2b99d6cd963afa
SEBASTIAO RODRIGO <rodrigoexer1@gmail.com>
2024-03-09 11:09:56 -0300

    coloquei para ficar responsivo atravez do bootstrap

--- src/views/Login/index.jsx
+++ src/views/Login/index.jsx
@@ -12,13 +12,13 @@ export function Login(){
 
     return(
         <div className="container text-center">
-            <div className="form col-6 mx-auto">
+            <div className="form">
                 <h1 className="mt-5 mb-5">Bem vindo</h1>
-                <div className="row">
-                    <div className="col">
+                <div className="row p-3">
+                    <div className="col-md-6 col-sm-12 mb-3">
                         <input type="text" className="form-control" placeholder="e-mail" />
                     </div>
-                    <div className="col">
+                    <div className="col-md-6 col-sm-12">
                         <input type="password" className="form-control text-center" placeholder="*******" />
                     </div>
                     <div className="row">
